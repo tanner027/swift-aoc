349ee1b1ecbf7ef4419868c6c614e8f650f23ff1
Curtis Couture <curtis.couture@gmail.com>
2024-12-07 20:55:23 -0500

    Slight optimization.

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -2,7 +2,7 @@ import Algorithms
 import Foundation
 
 struct Day04: AdventDay {
-  
+
   enum Day04Direction: String {
     case n
     case ne
@@ -13,14 +13,13 @@ struct Day04: AdventDay {
     case w
     case nw
   }
-  
-  
+
   enum Xmas: String {
     case X
     case M
     case A
     case S
-    
+
     func next() -> Xmas? {
       switch self {
       case .X: return .M
@@ -29,7 +28,7 @@ struct Day04: AdventDay {
       case .S: return nil
       }
     }
-    
+
     init(_ character: Character) {
       switch character {
       case "X": self = .X
@@ -40,12 +39,12 @@ struct Day04: AdventDay {
       }
     }
   }
-  
+
   var data: String
-  
+
   var wordSearchGRID: [[Xmas]] {
-    data.split(separator: "\n").map{
-      $0.map{Xmas.init($0)}
+    data.split(separator: "\n").map {
+      $0.map { Xmas.init($0) }
     }
   }
 
@@ -61,47 +60,74 @@ struct Day04: AdventDay {
     }
     return found
   }
-  
-  func findXmas(column: Int, row: Int, current: Xmas = .X, currentDirection: Day04Direction? = nil) -> Int {
+
+  func findXmas(
+    column: Int, row: Int, current: Xmas = .X,
+    currentDirection: Day04Direction? = nil
+  ) -> Int {
     guard let next = current.next() else { return 1 }
-    
-    let coords: [(Int, Int, Day04Direction)] = [
-      // top
-      (column, row + 1, .n),
-      // top left
-      (column - 1, row + 1, .nw),
-      // top right
-      (column + 1, row + 1, .ne),
-      // left
-      (column - 1, row, .w),
-      // right
-      (column + 1, row, .e),
-      // bottom
-      (column, row - 1, .s),
-      // bottom left
-      (column - 1, row - 1, .sw),
-      // bottom right
-      (column + 1, row - 1, .se),
+
+    //    let coords: [(Int, Int, Day04Direction)] = [
+    //      // top
+    //      (column, row + 1, .n),
+    //      // top left
+    //      (column - 1, row + 1, .nw),
+    //      // top right
+    //      (column + 1, row + 1, .ne),
+    //      // left
+    //      (column - 1, row, .w),
+    //      // right
+    //      (column + 1, row, .e),
+    //      // bottom
+    //      (column, row - 1, .s),
+    //      // bottom left
+    //      (column - 1, row - 1, .sw),
+    //      // bottom right
+    //      (column + 1, row - 1, .se),
+    //    ]
+    let coords: [Day04Direction: (Int, Int)] = [
+      .n: (column, row + 1),
+      .nw: (column - 1, row + 1),
+      .ne: (column + 1, row + 1),
+      .w: (column - 1, row),
+      .e: (column + 1, row),
+      .s: (column, row - 1),
+      .sw: (column - 1, row - 1),
+      .se: (column + 1, row - 1),
     ]
-    
-    // NOTE: since the grid dimensions are the same we can use wordSearchGRID.count here
-    // prone to cause bugs if input changes
-    let validCoords = coords.filter{ coord in
-      return coord.0 >= 0 && coord.0 < wordSearchGRID.count && coord.1 >= 0 && coord.1 < wordSearchGRID.count
-    }
-    
+
+
+
+
     var found = 0
-    for coord in validCoords {
-      let direction = coord.2
-      let previousDirection = currentDirection ?? direction
-      if wordSearchGRID[coord.1][coord.0] == next && direction == previousDirection {
-        found += findXmas(column: coord.0, row: coord.1, current: next, currentDirection: direction)
+    
+    // if we have a current direction, ie XM, only check for the
+    // next letter in valid coordindates
+    if let currentDirection = currentDirection {
+      let coord = coords[currentDirection, default: (0, 0)]
+      if coord.0 >= 0 && coord.0 < wordSearchGRID.count && coord.1 >= 0 && coord.1 < wordSearchGRID.count
+          && wordSearchGRID[coord.1][coord.0] == next {
+        found += findXmas(
+          column: coord.0, row: coord.1, current: next,
+          currentDirection: currentDirection)
+      }
+    }
+    // otherwise look for a valid next letter
+    else {
+      for coord in coords {
+        let direction = coord.key
+        let coord = coord.value
+        if coord.0 >= 0 && coord.0 < wordSearchGRID.count && coord.1 >= 0 && coord.1 < wordSearchGRID.count && wordSearchGRID[coord.1][coord.0] == next {
+          found += findXmas(
+            column: coord.0, row: coord.1, current: next,
+            currentDirection: direction)
+        }
       }
     }
     return found
   }
-  
+
   func part2() -> Int {
-0
+    0
   }
 }
